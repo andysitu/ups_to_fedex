f8f7ad06f6a91f815ec1a06a3a1986e0da5a6af5
Andy Situ <and.situ@gmail.com>
2017-04-18 17:40:15 -0700

    Implement new changes to main.py

--- main.py
+++ main.py
@@ -3,17 +3,16 @@ import ffile
 import make_excel
 
 import ups_reader_simple, ups_reader_detail
+from ups_data import *
 
 py_filename = "ups"
 
-
-def print_ups_data():
+def print_raw_ups_data():
 	for ups in ups_data:
 		print(ups_data[ups])
 
-
 def save_ups_data():
-	ffile.move_dir('ups_data')
+	ffile.move_dir('saved_data')
 
 	shelfFile = shelve.open(py_filename)
 	shelfFile['ups_data'] = ups_data
@@ -21,17 +20,25 @@ def save_ups_data():
 
 	ffile.dir_back()
 
-ups_data = ups_reader_simple.read('data/ups_simple.csv')
-ups_reader_detail.add_details('data/ups_detail.csv', ups_data)
+def convert_raw_data_to_data(raw_ups_data_dic, func):
+	for tracking_num, data in raw_ups_data_dic.items():
+		simple_data_list = data["simple"]
+		detail_data_super_list = data["detail"]
+		# print(tracking_num)
+		# print(simple_data_list)
+		# print(detail_data_super_list)
+		ups_data = UPS_Data(tracking_num, simple_data_list, detail_data_super_list)
+		print(ups_data)
 
-for tracking_num, data in ups_data.items():
-	print(tracking_num, data)
-	break
+raw_ups_data = ups_reader_simple.read('data/ups_simple.csv')
 
+ups_reader_detail.add_details('data/ups_detail.csv', raw_ups_data)
 
 # ups_data = ups_reader_detail.read('data/ups_detail.csv')
 
-make_excel.make(ups_data)
+make_excel.make(raw_ups_data)
+
+data = convert_raw_data_to_data(raw_ups_data)
 
 # print_upKs_data()
 # save_ups_data()
