7a6a09b4f90e5769f72b9db62bfcb798c139f2e8
Andy Situ <and.situ@gmail.com>
2017-04-19 12:06:14 -0700

    ffile - add save & open data functions

--- ffile.py
+++ ffile.py
@@ -1,4 +1,5 @@
 import os
+import shelve
 
 def move_dir(folder_name):
 	try:
@@ -9,3 +10,19 @@ def move_dir(folder_name):
 
 def dir_back():
 	os.chdir('..')
+
+def save_ups_data(ups_data):
+	move_dir("ups_data")
+
+	with shelve.open("ups_data") as db:
+		db["data"] = ups_data
+
+	dir_back()
+
+def open_ups_data():
+	move_dir("ups_data")
+
+	with shelve.open("ups_data") as db:
+		d = db["data"]
+
+	dir_back()
