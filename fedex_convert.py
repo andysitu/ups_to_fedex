45b0efeeb610f64803f761a5537cdfdfe3eb8b0c
Andy Situ <and.situ@gmail.com>
2017-04-19 18:11:36 -0700

    creation of fedex_convert.py

---
+++ fedex_convert.py
@@ -0,0 +1,22 @@
+service_level_index = {
+	"Ground Commercial": True,
+	"Ground Hundredweight": False,
+	"Ground Residential": True,
+	"2nd Day Air Commercial": True,
+	"2nd Day Air Residential": True,
+	"UPS SurePost - 1 LB or Greater": True,
+
+	"Worldwide Expedited": False,
+	"Ground Undeliverable Return": False,
+	# Probably Adjustments
+	"Residential": False,
+	# Probably Adjustments
+	"Ground": False,
+	# Usually adjustments
+	"Residential Surcharge": False,
+	"Expedited": False,
+	"Standard Shipment": False,
+	"Worldwide Express": False,
+	"Worldwide Standard": False,
+	"Ground Return to Sender": False,
+}
