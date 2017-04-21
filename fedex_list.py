5523a5034387b4bf7fb53ff6265b819f60282ba5
Andy Situ <and.situ@gmail.com>
2017-04-21 14:23:32 -0700

    check_service_level + misc
    * parameter name change of ups_rate_data to ups_rate_data_list
    * use check_service_level in add_data

--- fedex_list.py
+++ fedex_list.py
@@ -29,15 +29,25 @@ class Fedex_List():
 	}
 
 	fedex_data_dic = {}
-	def add_data(self, date, ups_tracking_num, ups_rate_data):
+	def add_data(self, date, ups_tracking_num, ups_rate_data_list):
 		if date not in self.fedex_data_dic:
 			self.fedex_data_dic[date] = {}
 		fx_d_dic = self.fedex_data_dic[date]
+		#Check if tracking_num exists. If so, then throw error in else.
 		if ups_tracking_num not in fx_d_dic:
-			f_d = Fedex_Data(date, ups_rate_data)
-			fx_d_dic[ups_tracking_num] = f_d
+			if self.check_service_level(ups_rate_data_list):
+				f_d = Fedex_Data(date, ups_rate_data_list)
+				fx_d_dic[ups_tracking_num] = f_d
 		else:
 			e_msg = "fedex list already has an existing"
 			e_msg += " data instance with ups "
 			e_msg += "tracking num of " + ups_tracking_num
 			raise Exception(e_msg)
+
+	def check_service_level(self, ups_rate_data_list):
+		for ups_rate_data in ups_rate_data_list:
+			service_level = ups_rate_data["simple"]["Service Level"]
+			if self.service_level_index[service_level]:
+				return True
+			else:
+				return False
