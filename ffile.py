cf4c81c004ed823c0a9433429342c94dc0622981
Andy Situ <and.situ@gmail.com>
2017-04-21 11:00:00 -0700

    dir_back has parameter num_times (to go back)

--- ffile.py
+++ ffile.py
@@ -2,6 +2,9 @@ import os
 import shelve
 
 def move_dir(folder_name):
+	# Uses os.chdir to move to a directory.
+	# If it doesn't exist (with FileNotFoundError),
+	# 	then the directory will be made.
 	try:
 		os.chdir('./' + folder_name)
 	except(FileNotFoundError):
@@ -10,6 +13,9 @@ def move_dir(folder_name):
 
 def dir_back():
 	os.chdir('..')
+def dir_back(num_times = 1):
+	for i in range(num_times):
+		os.chdir('..')
 
 def save_ups_data(ups_data):
 	move_dir("ups_data")
@@ -17,7 +23,7 @@ def save_ups_data(ups_data):
 	with shelve.open("ups_data") as db:
 		db["data"] = ups_data
 
-	dir_back()
+	dir_back(2)
 
 def open_ups_data():
 	move_dir("ups_data")
@@ -26,5 +32,4 @@ def open_ups_data():
 	with shelve.open("ups_data") as db:
 		d = db["data"]
 
-	dir_back()
-	return d
+	return d	dir_back(2)
