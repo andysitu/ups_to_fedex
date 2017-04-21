98825fa609330a20867b45c81d80e701e626a9c1
Andy Situ <and.situ@gmail.com>
2017-04-21 13:21:45 -0700

    moved service_level_index to fedex_list

--- fedex_converter.py
+++ fedex_converter.py
@@ -1,27 +1,3 @@
-service_level_index = {
-	"Ground Commercial": True,
-	"Ground Hundredweight": False,
-	"Ground Residential": True,
-	"2nd Day Air Commercial": True,
-	"2nd Day Air Residential": True,
-	"UPS SurePost - 1 LB or Greater": True,
-
-	"Worldwide Expedited": False,
-	"Ground Undeliverable Return": False,
-	# Probably Adjustments
-	"Residential": False,
-	# Probably Adjustments
-	"Ground": False,
-	# Usually adjustments
-	"Residential Surcharge": False,
-	#Adjustments
-	"Expedited": False,
-	"Standard Shipment": False,
-	"Worldwide Express": False,
-	"Worldwide Standard": False,
-	"Ground Return to Sender": False,
-}
-
 def convert(ups_data, fedex_list, max_service_level_num):
 	for date, data_track_num_obj in ups_data.items():
 		for track_num, ups_data_obj in data_track_num_obj.items():
