58c454d2a19b8b3f0da8a95001a6ab098ca3e9b8
Andy Situ <and.situ@gmail.com>
2017-04-21 14:15:09 -0700

    move service_level_index into Fedex_Data

--- fedex_list.py
+++ fedex_list.py
@@ -1,32 +1,32 @@
 from fedex_data import Fedex_Data
 
-service_level_index = {
-	"Ground Commercial": True,
-	"Ground Hundredweight": False,
-	"Ground Residential": True,
-	"2nd Day Air Commercial": True,
-	"2nd Day Air Residential": True,
-	"UPS SurePost - 1 LB or Greater": True,
-
-	"Worldwide Expedited": False,
-	"Ground Undeliverable Return": False,
-	# Probably Adjustments
-	"Residential": False,
-	# Probably Adjustments
-	"Ground": False,
-	# Usually adjustments
-	"Residential Surcharge": False,
-	#Adjustments
-	"Expedited": False,
-	"Standard Shipment": False,
-	"Worldwide Express": False,
-	"Worldwide Standard": False,
-	"Ground Return to Sender": False,
-}
-
 class Fedex_List():
 	# def __init__(self):
 	# 	self.pickup_date = pickup_date
+
+	service_level_index = {
+		"Ground Commercial": True,
+		"Ground Hundredweight": False,
+		"Ground Residential": True,
+		"2nd Day Air Commercial": True,
+		"2nd Day Air Residential": True,
+		"UPS SurePost - 1 LB or Greater": True,
+
+		"Worldwide Expedited": False,
+		"Ground Undeliverable Return": False,
+		# Probably Adjustments
+		"Residential": False,
+		# Probably Adjustments
+		"Ground": False,
+		# Usually adjustments
+		"Residential Surcharge": False,
+		#Adjustments
+		"Expedited": False,
+		"Standard Shipment": False,
+		"Worldwide Express": False,
+		"Worldwide Standard": False,
+		"Ground Return to Sender": False,
+	}
 
 	fedex_data_dic = {}
 	def add_data(self, date, ups_tracking_num, ups_rate_data):
