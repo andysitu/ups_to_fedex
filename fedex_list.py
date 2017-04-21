b3e6879f8d76acbd7706c05645cc47b9f940de31
Andy Situ <and.situ@gmail.com>
2017-04-21 13:00:57 -0700

    change data name to ups_rate_data, into Fedex_Data

--- fedex_list.py
+++ fedex_list.py
@@ -1,19 +1,18 @@
-import fedex_data
+from fedex_data import Fedex_Data
 
 class Fedex_List():
 	# def __init__(self):
 	# 	self.pickup_date = pickup_date
 
 	fedex_data_dic = {}
-	def add_data(self, date, ups_tracking_num, data):
-		if date not in fedex_data_dic:
-			fedex_data_date_dic[date] = {}
-		fx_d_dic = fedex_data_dic[date]
-		if ups_tracking_num not in fedex_data_date_dic:
-			fx_d_dic[ups_tracking_num] = Fedex_Data(date)
+	def add_data(self, date, ups_tracking_num, ups_rate_data):
+		if date not in self.fedex_data_dic:
+			self.fedex_data_dic[date] = {}
+		fx_d_dic = self.fedex_data_dic[date]
+		if ups_tracking_num not in fx_d_dic:
+			fx_d_dic[ups_tracking_num] = Fedex_Data(date, ups_rate_data)
 		else:
 			e_msg = "fedex list already has an existing"
 			e_msg += " data instance with ups "
 			e_msg += "tracking num of " + ups_tracking_num
 			raise Exception(e_msg)
-
