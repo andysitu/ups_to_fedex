5f910b2bf5759ba0bef2a36d06d61b343e784346
Andy Situ <and.situ@gmail.com>
2017-04-21 13:20:46 -0700

    convert adds data to fedex_list

--- fedex_converter.py
+++ fedex_converter.py
@@ -22,9 +22,8 @@ service_level_index = {
 	"Ground Return to Sender": False,
 }
 
-def convert(ups_data):
+def convert(ups_data, fedex_list, max_service_level_num):
 	for date, data_track_num_obj in ups_data.items():
-		for track_num, data_obj in data_track_num_obj.items():
-			data_obj.get_rate_data()
-
-
+		for track_num, ups_data_obj in data_track_num_obj.items():
+			formatted_ups_data = ups_data_obj.get_rate_data()
+			fedex_list.add_data(date, track_num, formatted_ups_data)
