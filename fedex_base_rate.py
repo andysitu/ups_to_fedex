500527abda529b18d768e65a775c726fbebef537
Andy Situ <and.situ@gmail.com>
2017-04-27 18:34:02 -0700

    calculate rates & output excel file

--- fedex_base_rate.py
+++ fedex_base_rate.py
@@ -6,7 +6,7 @@ column_letters = [
 	'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
 ]
 
-def convert_file(filename, folder_name):
+def convert_file(filename, folder_name, new_filename ='fedex_rates.xlsx', annual_charge = 0.00):
 	""" 
 	Opens an excel file containing standard base Fedex rates and will
 		convert it to the appropriate values based on the discount
@@ -21,16 +21,12 @@ def convert_file(filename, folder_name):
 
 	wb = openpyxl.load_workbook(filename)
 
-
-
 	sheet_names_list = wb.get_sheet_names()
 
 	for sheet_name in sheet_names_list:
-		match = match_sheet_names(sheet_name)
-
-	for sheet_name in sheet_names_list:
-		print(sheet_name)
 		sheet = wb.get_sheet_by_name(sheet_name)
+
+		calc_func = match_sheet_names(sheet_name)
 
 		max_rows = sheet.max_row
 		max_columns = sheet.max_column
@@ -44,23 +40,43 @@ def convert_file(filename, folder_name):
 				continue
 			zone_dic = find_zones(zone_value)
 			process_column_zone_index(column_index, zone_dic, letter)
-		print(column_index)
 
+		for row_num in range(2, max_rows + 1):
+			row = str(row_num)
 
+			weight = sheet[weight_column + row].value
+			for zone, column_letter in column_index.items():
+				if zone != "weight":
+					cell_loc = column_letter + row
+					rate = sheet[cell_loc].value
+
+					#blank rates are returned back
+					if rate == '-':
+						new_rate = rate
+					else:
+						new_rate = process_rate(calc_func, zone, weight, rate, annual_charge)
+					# print(rate, new_rate)
+
+					sheet[cell_loc] = new_rate
+				else:
+					# print("weight: " + str(sheet[column_letter + row].value))
+					continue
+
+	wb.save(new_filename)
 	ffile.dir_back()
 
 def match_sheet_names(sheet_name):
 	s_name = sheet_name.lower()
 	sheet_name_regex_delivery_type_index = {
-		r"priority overnight": 0,
-		r'standard overnight': 1,
-		r'2 day am': 2,
-		r'2 day': 3,
-		r'express saver': 4,
-		r'ground': 5,
-		r'home': 6,
-		r'smart post.*1-16 oz': 7,
-		r'smart post.*lbs': 8,
+		r"priority overnight": calc_pri_overnight,
+		r'standard overnight': calc_overnight,
+		r'2 day am': calc_2day_am,
+		r'2 day': calc_2day,
+		r'express saver': calc_express_saver,
+		r'ground': calc_ground,
+		r'home': calc_ground,
+		r'smart post.*1-16 oz': calc_smartpost_oz,
+		r'smart post.*lbs': calc_smartpost_lb,
 	}
 
 	for regex in sheet_name_regex_delivery_type_index:
@@ -97,10 +113,39 @@ def process_column_zone_index(column_index, zone_dic, col_letter):
 	"""
 	zone_dic refers to the dictionary returned by find_zones
 	Indexes key to zone number and value to column letter.
-	If mult_zones (ex: 4-15) will have each zone corresponding
-		to the same column letter.
+	Only start will be used as index for column index
 	"""
 	start = int(zone_dic['start'])
+	column_index[start] = col_letter
+
+	# if "end" not in zone_dic:
+	# 	column_index[start] = col_letter
+	# else:
+	# 	end = int(zone_dic['end'])
+	# 	for zone_num in range(start, end + 1):
+	# 		column_index[zone_num] = col_letter
+
+def process_rate(calc_func, zone, weight, full_rate, annual_charge):
+	type_status = False
+
+	zone = int(zone)
+	full_rate = float(full_rate)
+	#convert all parameters into right type
+	try:
+		weight = int(weight)
+	except ValueError:
+		weight = weight.lower()
+		env_re = r"envelope"
+		over_re = r'oversize'
+		if re.search(env_re, weight) != None:
+			type_status = "weight"
+		elif re.search(over_re, weight) != None:
+			type_status = "oversize"
+	# print(full_rate)
+	new_rate = calc_func(zone, weight, full_rate, annual_charge, type_status)
+	return new_rate
+
+
 #Functions to calculate the discounted rates
 def calc_earned_discount(annual_charge, ground_or_home = False):
 	annual_charge = float(annual_charge)
@@ -268,8 +313,6 @@ def calc_smartpost_lb(zone, weight, full_rate, annual_charge, type_status):
 		new_rate = test_min_charge(new_rate, "smartpost_lb_9_10_17_26_99")
 	return new_rate
 
-	if "end" not in zone_dic:
-		column_index[start] = col_letter
 min_charges_dic = {
 # POSSIBILITY OF AUTOMATING THIS
 	"priority_overnight_envelope": 10.50,
