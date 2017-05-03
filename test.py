2bc6ad7d7160c5c5ca0a5631478458d2aed456ef
Andy Situ <and.situ@gmail.com>
2017-05-03 16:55:29 -0700

    add more test cases

--- test.py
+++ test.py
@@ -9,4 +9,13 @@ def test_equal(val1, val2):
 
 test_equal(excel_helper.get_column_num("AAA"), 703)
 test_equal(excel_helper.get_column_letters(1), 'A')
-test_equal(excel_helper.get_column_letters(16), 'Q')
+test_equal(excel_helper.get_column_letters(16), 'P')
+
+# Test zones
+test_equal(excel_helper.get_zone("Zone 5")["start"], "5")
+test_equal(excel_helper.get_zone("Zone 5")["end"], "5")
+test_equal(excel_helper.get_zone("Zone 5-10")["start"], "5")
+test_equal(excel_helper.get_zone("Zone 5-10")["end"], "10")
+test_equal(excel_helper.get_zone("Zone 5 - 10")["start"], "5")
+test_equal(excel_helper.get_zone("Zone 5 - 10")["end"], "10")
+test_equal(excel_helper.get_zone("5")["start"], "5")
