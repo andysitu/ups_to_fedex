0dfdfec07ab178ce45150d1bf71398a6ce7e5750
Andy Situ <and.situ@gmail.com>
2017-05-03 17:38:50 -0700

    get weight from excel file

--- src/fedex_base_rates.py
+++ src/fedex_base_rates.py
@@ -9,8 +9,15 @@ def open_file(filename, folder_name):
 
 	for sheet_name in sheet_names:
 		sheet = wb.get_sheet_by_name(sheet_name)
-		get_zones(sheet)
+		zone_dic = excel_helper.get_zones(sheet)
+		# print(zone_dic)
 
+		num_rows = sheet.max_row
+		weight_column = excel_helper.weight_column
+		for row_num in range( int(excel_helper.zone_row) + 1, num_rows + 1):
+			weight = sheet[weight_column + str(row_num)].value
+			print(weight)
+			# for col_letter, zone
 	ffile.dir_back()
 
 def save_file(filename):
