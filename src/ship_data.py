5661c26b5ba47afe880d6de7a27f5340eff3efec
Andy Situ <and.situ@gmail.com>
2017-05-08 16:08:15 -0700

    Ship_Data init sync (WIP)

--- src/ship_data.py
+++ src/ship_data.py
@@ -1,3 +1,16 @@
+import re
+import locale
+
 class Ship_Data():
-	def test():
-		print("HELLO")
+	def __init__(self, simple_ups_data_list, detail_ups_data_list):
+		# print(simple_ups_data_list)
+		# print(detail_ups_data_list)
+
+		locale.setlocale(locale.LC_ALL, 'English_United States.1252')
+
+		for simple_data in simple_ups_data_list:
+			charge_value = simple_data["billed_charge"]
+			charge_re = re.compile(r"\d+\.\d+")
+			charge = charge_re.search(charge_value)
+			# print(locale.currency(charge_value, grouping=True))
+			print(charge_value, charge[0])
