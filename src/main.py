87e9c1e01ea683e3552b4390bd60805f15f4f4fd
Andy Situ <and.situ@gmail.com>
2017-05-08 14:21:42 -0700

    chng name process_ups_data & add date_string

--- src/main.py
+++ src/main.py
@@ -1,7 +1,6 @@
 from . import reader
 from src import ship_data_handler
 
-def read_ups_data():
 	simple_ups_data = reader.read_simple_ups("041517 ups_simple.csv", "ups_invoices")
 	detail_ups_data = reader.read_detail_ups("041517 ups_detail.csv", "ups_invoices")
 	for tracking_num, detail_list in detail_ups_data.items():
@@ -9,9 +8,9 @@ def read_ups_data():
 		# 	print("OK")
 		# print(len(detail_list))
 		pass
+def process_ups_data(date_string):
 	s_data_handler = ship_data_handler.Ship_Data_Handler()
 
 	s_data_handler.process(simple_ups_data, detail_ups_data)
 
-
-read_ups_data()
+process_ups_data("032517")
