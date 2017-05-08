14ce7eddb7ae3f98af4d2211f10ac93bbc20579f
Andy Situ <and.situ@gmail.com>
2017-05-08 14:35:43 -0700

    s_d_handler.process creates new ship_data

--- src/ship_data_handler.py
+++ src/ship_data_handler.py
@@ -1,13 +1,19 @@
+from . import ship_data
+
 class Ship_Data_Handler():
     def __init__(self):
+        self._ship_data_dic = {}
         pass
 
-    def process(self, simple_ups_data, detail_ups_detail):
-        # for track_num, ups_data_dic in simple_ups_data.items():
-        #     print(ups_data_dic)
+    def process(self, total_simple_ups_data, total_detail_ups_detail):
+        for track_num, simple_ups_data_list in total_simple_ups_data.items():
+            detail_ups_data_list = total_detail_ups_detail[track_num]
+
+            if self.filter_ship_data(simple_ups_data_list, detail_ups_data_list):
+                ship_data.Ship_Data(simple_ups_data_list, detail_ups_data_list)
         # for track_num, ups_data_dic in detail_ups_detail.items():
-        #     print(ups_data_dic)
-        pass
+        #     print(simple_ups_data[track_num])
+
     def filter_ship_data(self, simple_ups_data_list, detail_ups_data_list):
     # Returns True if the Ship_Data instance should be added.
         return True
