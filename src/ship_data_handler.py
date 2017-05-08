20debd85558fbf6ad0a731d20e40a867d9d5e8f3
Andy Situ <and.situ@gmail.com>
2017-05-08 12:06:28 -0700

    create ship_data_handler

---
+++ src/ship_data_handler.py
@@ -0,0 +1,10 @@
+class Ship_Data_Handler():
+    def __init__(self):
+        pass
+
+    def process(self, simple_ups_data, detail_ups_detail):
+        # for track_num, ups_data_dic in simple_ups_data.items():
+        #     print(ups_data_dic)
+        # for track_num, ups_data_dic in detail_ups_detail.items():
+        #     print(ups_data_dic)
+        pass
