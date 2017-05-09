aa20cf5a24de9cc52707e00f784207628dcef0a2
Andy Situ <and.situ@gmail.com>
2017-05-08 18:01:40 -0700

    match_simple_and_detail_ups func + neg value

--- src/ship_data.py
+++ src/ship_data.py
@@ -1,16 +1,21 @@
 import re
-import locale
 
 class Ship_Data():
 	def __init__(self, simple_ups_data_list, detail_ups_data_list):
 		# print(simple_ups_data_list)
 		# print(detail_ups_data_list)
 
-		locale.setlocale(locale.LC_ALL, 'English_United States.1252')
+		self.match_simple_and_detail_ups(simple_ups_data_list, detail_ups_data_list)
 
+
+	def match_simple_and_detail_ups(self, simple_ups_data_list, detail_ups_data_list):
 		for simple_data in simple_ups_data_list:
-			charge_value = simple_data["billed_charge"]
-			charge_re = re.compile(r"\d+\.\d+")
-			charge = charge_re.search(charge_value)
-			# print(locale.currency(charge_value, grouping=True))
-			print(charge_value, charge[0])
+			bill_charge_value = simple_data["billed_charge"]
+			# charge_re = re.compile(r"(\-*).*(\d+\.\d+)")
+			bill_charge_re = re.compile(r"\d+\.\d+")
+			charge = ( bill_charge_re.search(bill_charge_value) )[0]
+
+			if bill_charge_value[0] == '-':
+				self.total_bill_charge = -float(charge)
+			else:
+				self.total_bill_charge = float(charge)
