55ed0b34579f021a6e32a12ba9944039675291bd
Andy Situ <and.situ@gmail.com>
2017-05-10 15:03:17 -0700

    change parameter nameedit out match_simple & detail

--- src/ship_data.py
+++ src/ship_data.py
@@ -1,16 +1,14 @@
 import re
+from . import ups_ship_data
 
 class Ship_Data():
-	def __init__(self, simple_ups_data_list, detail_ups_data_list):
+# Assume that only outbound charges will be used, meaning no adjustments.
+	def __init__(self, simple_ups_data_list, total_detail_ups_data_list):
 		# print(simple_ups_data_list)
-		# print(detail_ups_data_list)
+		# print(total_detail_ups_data_list)
 
-		self.match_simple_and_detail_ups(simple_ups_data_list, detail_ups_data_list)
+		self.create_ups_ship_data(simple_ups_data_list, total_detail_ups_data_list)
 
-	def match_simple_and_detail_ups(self, simple_ups_data_list, detail_ups_data_list):
-		for simple_data in simple_ups_data_list:
-			bill_charge_value = simple_data["billed_charge"]
-			total_bill_charge = self.convert_charge_string_to_float(bill_charge_value)
-
-			print(total_bill_charge)
-
+	def match_simple_and_detail_ups(self, simple_ups_data_list, total_detail_ups_data_list):
+		# Not used since anything with length > 1 is filtered out in ship_data_handler
+		length_list =len(simple_ups_data_list)
