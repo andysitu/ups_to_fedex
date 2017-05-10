f765bed8cb40342b828b328dfce26cc73c776165
Andy Situ <and.situ@gmail.com>
2017-05-10 14:41:07 -0700

    filter all invoice sections except Outbound

--- src/ship_data_handler.py
+++ src/ship_data_handler.py
@@ -16,4 +16,22 @@ class Ship_Data_Handler():
 
     def filter_ship_data(self, simple_ups_data_list, detail_ups_data_list):
     # Returns True if the Ship_Data instance should be added.
-        return True
+
+    # Filter any adjustments or anything without adjustments.
+
+        if len(simple_ups_data_list) > 1:
+            return False
+
+        for simple_ups_data in simple_ups_data_list:
+            invoice_section = simple_ups_data["invoice_section"]
+            if invoice_section== "Outbound":
+                return True
+            elif invoice_section == "Adjustments & Other Charges":
+                return False
+            elif invoice_section == "Adjustments":
+                return False
+            elif invoice_section == "Void Credits":
+                return False
+            else:
+                msg = "filter_ship_data of Ship_Data_Handler seen unknown invoice section " + invoice_section
+                raise Exception(msg)
