5b444b2f077722b6aa0bafc2292603f2dae047fc
Andy Situ <and.situ@gmail.com>
2017-05-10 15:03:26 -0700

    create_ups_ship_data method

--- src/ship_data.py
+++ src/ship_data.py
@@ -12,3 +12,8 @@ class Ship_Data():
 	def match_simple_and_detail_ups(self, simple_ups_data_list, total_detail_ups_data_list):
 		# Not used since anything with length > 1 is filtered out in ship_data_handler
 		length_list =len(simple_ups_data_list)
+
+	def create_ups_ship_data(self, simple_ups_data_list, total_detail_ups_data_list):
+		simple_ups_data = ups_ship_data.Simple_UPS_Ship_Data(simple_ups_data_list)
+		detail_ups_data = ups_ship_data.Detail_UPS_Ship_Data(total_detail_ups_data_list)
+		pass
