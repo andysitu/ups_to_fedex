4db1d65a0559ed2f6cb38ea8e21acb956a87b073
Andy Situ <and.situ@gmail.com>
2017-05-12 16:57:26 -0700

    fedex_ship_data takes single parameters (not dic)

--- src/fedex_ship_data.py
+++ src/fedex_ship_data.py
@@ -1,16 +1,15 @@
 class Simple_Fedex_Ship_Data():
-    def __init__(self, simple_ups_data):
-        self.total_bill_charge = simple_ups_data["billed_charge"]
+    def __init__(self, service_level, weight, zone, pickup_date):
+        # self.total_bill_charge = simple_ups_data["billed_charge"]
         # self.invoice_section = simple_ups_data["invoice_section"]
-        self.service_level = simple_ups_data["service_level"]
-        self.weight = simple_ups_data["weight"]
-        self.zone = simple_ups_data["zone"]
-        self.pickup_date = simple_ups_data["pickup_date"]
+        self.service_level = service_level
+        self.weight = weight
+        self.zone = zone
+        self.pickup_date = pickup_date
         # self.incentive_credit = excel_helper.convert_charge_string_to_float(simple_ups_data["incentive_credit"])
         # self.invoice_date = simple_ups_data["invoice_date"]
 
 class Detail_Fedex_Ship_Data():
-    def __init__(self, detail_ups_data):
-        self.charge_type = detail_ups_data["charge_type"]
-        self.billed_charge = detail_ups_data["billed_charge"]
-        self.incentive_credit = detail_ups_data["incentive_credit"]
+    def __init__(self, charge_type):
+        self.charge_type = charge_type
+        # self.billed_charge = detail_ups_data["billed_charge"]
