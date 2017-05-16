ff55f528e3df1ce2c4e381ba0d005afe7efef622
Andy Situ <and.situ@gmail.com>
2017-05-16 15:30:50 -0700

    process_ups_data uses strings for month, day, year

--- src/main.py
+++ src/main.py
@@ -1,7 +1,10 @@
 from . import reader
 from src import ship_data_handler
+from src import fedex_rates
+from src import excel_maker
 
-def process_ups_data(date_string):
+def process_ups_data(month_string, day_string, year_string, rate_dic):
+	date_string = month_string + day_string + year_string
 	folder_name = "ups_invoices"
 	simple_ups_filename = date_string + " ups_simple.csv"
 	detail_ups_filename = date_string + " ups_detail.csv"
@@ -17,4 +20,11 @@ def process_ups_data(date_string):
 
 	s_data_handler.process(total_simple_ups_data, total_detail_ups_data, rate_dic)
 
-process_ups_data("032517")
+	return s_data_handler
+
+def get_fedex_rates(earned_discount = 0):
+	rate_dic = fedex_rates.process_excel_fedex(0)
+
+	s_data_handler = process_ups_data("03", "25", "17", rate_dic)
+
+get_fedex_rates()
