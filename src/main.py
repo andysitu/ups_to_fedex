9675458a0a396d6b2c798f6b7740cb481fc5b78d
Andy Situ <and.situ@gmail.com>
2017-05-16 16:31:48 -0700

    remove fedex_rate_dic in creation of ship_handler

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from src import ship_data_handler
 from src import fedex_rates
 from src import excel_maker
 
-def process_ups_data(month_string, day_string, year_string, rate_dic):
+def process_ups_data(month_string, day_string, year_string):
 	date_string = month_string + day_string + year_string
 	folder_name = "ups_invoices"
 	simple_ups_filename = date_string + " ups_simple.csv"
@@ -16,13 +16,13 @@ def process_ups_data(month_string, day_string, year_string, rate_dic):
 	# 	print(len(detail_list))
 	# 	print(detail_list)
 	# 	pass
-	s_data_handler = ship_data_handler.Ship_Data_Handler(date_string, total_simple_ups_data, total_detail_ups_data, rate_dic)
+	s_data_handler = ship_data_handler.Ship_Data_Handler(date_string, total_simple_ups_data, total_detail_ups_data)
 
 	return s_data_handler
 
 def get_fedex_rates(earned_discount = 0):
 	rate_dic = fedex_rates.process_excel_fedex(0)
 
-	s_data_handler = process_ups_data("03", "25", "17", rate_dic)
+	s_data_handler = process_ups_data("03", "25", "17")
 
 get_fedex_rates()
