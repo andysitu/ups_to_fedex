f6d63182a6fc1f922b13116c3153faa6d5378d50
Andy Situ <and.situ@gmail.com>
2017-05-16 17:23:00 -0700

    make_excel_rate_file - WIP

--- src/main.py
+++ src/main.py
@@ -2,6 +2,8 @@ from . import reader
 from src import ship_data_handler
 from src import fedex_rates
 from src import excel_maker
+import openpyxl
+import ffile
 
 def process_ups_data(month_string, day_string, year_string):
 	date_string = month_string + day_string + year_string
@@ -37,5 +39,30 @@ def get_rates(s_data_handler, earned_discount = 0):
 		# print(u)
 		# print(f)
 
+s_data_handler = process_ups_data("03", "25", "17")
+get_rates(s_data_handler, 2)
 
-get_rates()
+def make_excel_rates_file(s_data_handler, excel_filename, foldername):
+	invoice_date_list = ["1", "2", "3"]
+	header_list = ["Date", "Zone", "Weight", "UPS", "Track Num/Rate", "Fedex", "FX Rate"]
+
+	data_dict_for_make_excel = {}
+
+	for invoice_date in invoice_date_list:
+		excel_data_list = []
+		excel_data_list.append(header_list)
+		data_dict_for_make_excel[invoice_date] = excel_data_list
+
+	def change_sheet_function(sheet):
+		sheet.freeze_panes = 'A2'
+
+		UPS_column_width = 30
+		tracking_num_width = 20
+		fedex_column_width = 30
+		sheet.column_dimensions['D'].width = UPS_column_width
+		sheet.column_dimensions['E'].width = tracking_num_width
+		sheet.column_dimensions['F'].width = fedex_column_width
+
+	excel_maker.make_excel_file(data_dict_for_make_excel, excel_filename, foldername, change_sheet_function)
+
+make_excel_rates_file(s_data_handler, "test.xlsx", "test")
