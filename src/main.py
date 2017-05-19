4437ef30a15fa0675b2489b6305a08a07f76c380
Andy Situ <and.situ@gmail.com>
2017-05-18 18:39:07 -0700

    update make_excel_file (functional from before)

--- src/main.py
+++ src/main.py
@@ -2,6 +2,8 @@ from . import reader
 from src import ship_data_handler
 from src import fedex_rates
 from src import excel_maker
+
+from inspect import signature
 import openpyxl
 import ffile
 
@@ -39,49 +41,136 @@ def get_rates(s_data_handler, earned_discount = 0):
 		# print(u)
 		# print(f)
 
-s_data_handler = process_ups_data("03", "25", "17")
-get_rates(s_data_handler, 2)
-
 def make_excel_rates_file(s_data_handler, excel_filename, foldername):
 	max_earned_discount_num = 5
 
-	header_list = []
-	header_dic = {"Date": 1, "Zone": 2, "Weight": 3, "UPS":4,
-				  "Track Num/Rate": 5, "Fedex": 6, "Fedex Rate": 7,
-				  "Diff": 8, "Diff Amount": 9}
+	def make_excel_data_list(header_list, data_dic):
+		data_list = []
+		for header in header_list:
+			data_list.append(data_dic[header])
+		return data_list
 
-	date_col_num = header_dic["Date"]
-	zone_col_num = header_dic["Zone"]
-	weight_col_num = header_dic["Weight"]
-	ups_col_num = header_dic["UPS"]
-	ups_rate_col_num = header_dic["Track Num/Rate"]
-	fedex_col_num = header_dic["Fedex"]
-	fedex_rate_col_num = header_dic["Fedex Rate"]
-	diff_word_col_num = header_dic["Diff"]
-	diff_amount_col_num = header_dic["Diff Amount"]
+	def make_data_dic(date="",zone="",weight= "",
+					  ups="",ups_rate ="",fedex="",
+					  fedex_rate="",diff="",diff_amnt=""):
+		return {
+			"Date": date,
+			"Zone": zone,
+			"Weight": weight,
+			"UPS": ups,
+			"Track Num/ Rate": ups_rate,
+			"Fedex": fedex,
+			"Fedex Rate": fedex_rate,
+			"Diff": diff,
+			"Diff Amount": diff_amnt,
+		}
 
-	# Fill out header_list
-	for i in range(1, len(header_dic) + 1):
-		for header in header_dic:
-			if header_dic[header] == i:
-				header_list.append(header)
+	def make_header_dic(data_dic_func):
+		sig = signature(data_dic_func)
+		params = sig.parameters
+		arg_length = len(params)
+		num_list = list(range(1,arg_length+1))
+		header_dic = make_data_dic(*num_list)
+		return header_dic
+
+	def make_header_list(header_dic):
+		header_list = []
+		for i in range(1, len(header_dic) + 1):
+			for header in header_dic:
+				if header_dic[header] == i:
+					header_list.append(header)
+		return header_list
+
+	header_dic = make_header_dic(make_data_dic)
+	header_list = make_header_list(header_dic)
+
+	def create_and_add_data_list():
+		title_dic = make_data_dic(date=item_info["pickup_date"], zone=item_info["zone"],
+								  weight=item_info["weight"], ups="UPS",
+								  ups_rate=track_num, fedex="Fedex", )
+		title_list = make_excel_data_list(header_list, title_dic)
+		excel_data_list.append(title_list)
 
 	track_num_list = s_data_handler.track_num_index
 
 	data_dict_for_make_excel = {}
 
+	#
 	for earned_discount_num in range(max_earned_discount_num + 1):
+		total_ups_billed_charge = 0.00
+		total_fedex_billed_charge = 0.00
+
 		fedex_rate_dic = fedex_rates.process_excel_fedex(earned_discount_num)
 		excel_data_list = []
 		excel_data_list.append(header_list)
 
 		for track_num in track_num_list:
-			f = get_fedex_rate_data(s_data_handler, track_num, fedex_rate_dic)
-			u = get_ups_rate_data(s_data_handler, track_num)
+			total_fedex_data_list = get_fedex_rate_data(s_data_handler, track_num, fedex_rate_dic)
+			total_ups_data_list = get_ups_rate_data(s_data_handler, track_num)
+
+			for num_id,ups_data_list in enumerate(total_ups_data_list):
+				item_info = s_data_handler.get_ship_data_info(track_num, num_id)
+				fedex_data_list = total_fedex_data_list[num_id]
+
+				title_dic = make_data_dic(date=item_info["pickup_date"],zone=item_info["zone"],
+									   weight=item_info["weight"],ups="UPS",
+									   ups_rate=track_num,fedex="Fedex",)
+				title_list = make_excel_data_list(header_list, title_dic)
+				excel_data_list.append(title_list)
+
+				ups_charges = 0
+				fedex_charges = 0
+
+				for i, ups_data_dic in enumerate(ups_data_list):
+					fedex_data_dic = fedex_data_list[i]
+					u_charge_type = ups_data_dic["charge_type"]
+					u_charge = ups_data_dic["billed_charge"]
+
+					f_charge_type = fedex_data_dic["charge_type"]
+					f_charge = fedex_data_dic["billed_charge"]
+
+
+					ups_charges += u_charge
+					fedex_charges += f_charge
+					total_ups_billed_charge += u_charge
+					total_fedex_billed_charge += f_charge
+
+
+					charges_dic = make_data_dic(ups=u_charge_type,ups_rate=u_charge,
+											  fedex=f_charge_type,fedex_rate=f_charge )
+					charges_list = make_excel_data_list(header_list, charges_dic)
+					excel_data_list.append(charges_list)
+
+				charges_dic = make_data_dic(ups="UPS TOTAL", ups_rate=ups_charges,
+											fedex="FEDEX TOTAL", fedex_rate=fedex_charges,
+											diff="Difference", diff_amnt=ups_charges-fedex_charges)
+				charges_list = make_excel_data_list(header_list, charges_dic)
+				excel_data_list.append(charges_list)
+
+
+
 
 		sheetname = str(earned_discount_num) + " earned discount"
+		# Row 2 for total UPS
+		excel_data_list[1].append("Total UPS")
+		excel_data_list[1].append(total_ups_billed_charge)
+
+		# Row 3 for total Fedex
+		excel_data_list[2].append("Total Fedex")
+		excel_data_list[2].append(total_fedex_billed_charge)
+
+		# Row 4 for difference
+		ups_fed_diff = total_ups_billed_charge - total_fedex_billed_charge
+		excel_data_list[3].append("Tot. Difference")
+		excel_data_list[3].append(ups_fed_diff)
+
+		# Row 5 for difference
+		excel_data_list[4].append("% Diff. from UPS")
+		excel_data_list[4].append(ups_fed_diff / total_ups_billed_charge)
+
 		data_dict_for_make_excel[sheetname] = excel_data_list
 
+		# make_excel_data_list(header_list, data_dict_for_make_excel)
 
 	def change_sheet_function(sheet):
 		sheet.freeze_panes = 'A2'
@@ -95,4 +184,3 @@ def make_excel_rates_file(s_data_handler, excel_filename, foldername):
 
 	excel_maker.make_excel_file(data_dict_for_make_excel, excel_filename, foldername, change_sheet_function)
 
-make_excel_rates_file(s_data_handler, "test.xlsx", "test")
