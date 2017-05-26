9464a56ef9de3570eaa3d3a6e8544a1b2ef205e0
Andy Situ <and.situ@gmail.com>
2017-05-26 16:24:13 -0700

    remove int conversion

--- src/fedex_ship_data.py
+++ src/fedex_ship_data.py
@@ -3,8 +3,8 @@ class Simple_Fedex_Ship_Data():
         # self.total_bill_charge = simple_ups_data["billed_charge"]
         # self.invoice_section = simple_ups_data["invoice_section"]
         self.service_level = service_level
-        self.weight = int(weight)
-        self.zone = int(zone)
+        self.weight = weight
+        self.zone = zone
         self.pickup_date = pickup_date
         # self.incentive_credit = excel_helper.convert_charge_string_to_float(simple_ups_data["incentive_credit"])
         # self.invoice_date = simple_ups_data["invoice_date"]
