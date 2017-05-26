28eda78766682e7d06b2b335c8ba24e2769c80b2
Andy Situ <and.situ@gmail.com>
2017-05-26 16:24:23 -0700

    add int conversion

--- src/ups_ship_data.py
+++ src/ups_ship_data.py
@@ -6,8 +6,8 @@ class Simple_UPS_Ship_Data():
         self.total_bill_charge = simple_ups_data["billed_charge"]
         self.invoice_section = simple_ups_data["invoice_section"]
         self.service_level = simple_ups_data["service_level"]
-        self.weight = simple_ups_data["weight"]
-        self.zone = simple_ups_data["zone"]
+        self.weight = int(simple_ups_data["weight"])
+        self.zone = int(simple_ups_data["zone"])
         self.pickup_date = simple_ups_data["pickup_date"]
         self.incentive_credit = excel_helper.convert_charge_string_to_float(simple_ups_data["incentive_credit"])
         self.invoice_date = simple_ups_data["invoice_date"]
